422d0603f252b9d35927cce61b677ad5ee1be1db
Wesley <wesleyrcardona@umes.edu.gt>
2026-07-30 22:52:09 -0600

    refactor: nombres y constantes

--- comisiones.py
+++ comisiones.py
@@ -3,7 +3,7 @@
 # hecho por kevin, no tocar, ya funciona
  
 # lista de vendedores
-ds = [
+vendedores = [
     ("María López", 45000.00),
     ("Carlos Pérez", 28500.00),
     ("Ana García", 61200.00),
@@ -11,37 +11,37 @@ ds = [
     ("Lucía Morales", 33400.00),
 ]
  
-def calc():
-    tp = 0
+def calcularComisiones():
+    totalAPagar = 0
     print("=" * 44)
     print("    COMISIONES DEL MES - LA COMERCIAL")
     print("=" * 44)
     # recorre la lista
-    for d in ds:
+    for d in vendedores:
         # si vendio mas de 30000
         if d[1] > 30000:
             # calcula la comision del 8%
-            c = d[1] * 0.08
-            c = round(c, 2)
+            comision = d[1] * 0.08
+            comision = round(comision, 2)
             # el bono es de 300
             if d[1] > 50000:
-                b = 500
+                bono = 500
             else:
-                b = 0
-            tot = round(c + b, 2)
-            tp = tp + tot
-            print(d[0] + ": Q " + str(tot))
+                bono = 0
+            totalVendedor = round(comision + bono, 2)
+            totalAPagar = totalAPagar + totalVendedor
+            print(d[0] + ": Q " + str(totalVendedor))
         else:
             # calcula la comision del 5%
-            c = d[1] * 0.05
-            c = round(c, 2)
-            b = 0
-            tot = round(c + b, 2)
-            tp = tp + tot
-            print(d[0] + ": Q " + str(tot))
+            comision = d[1] * 0.05
+            comision = round(comision, 2)
+            bono = 0
+            totalVendedor = round(comision + bono, 2)
+            totalAPagar = totalAPagar + totalVendedor
+            print(d[0] + ": Q " + str(totalVendedor))
     # ta = tp * 1.12
     # print("con iva", ta)
     print("-" * 44)
-    print("Total a pagar: Q " + str(round(tp, 2)))
+    print("Total a pagar: Q " + str(round(totalAPagar, 2)))
  
-calc()
+calcularComisiones()
